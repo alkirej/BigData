cce940d0f56b8dee723cf760df06222f8ed8e4c3
alkirej <jeff@alkire.net>
2020-01-28 19:45:29 -0500

    change /usr directory to /user to match JD's assignment

--- 012-HadoopCreateDir/hadoopDirCreate.sh
+++ 012-HadoopCreateDir/hadoopDirCreate.sh
@@ -4,7 +4,7 @@ start-yarn.sh
 
 echo CREATE /tmp  /usr and /data  directories in hdfs
 hdfs dfs -mkdir /tmp
-hdfs dfs -mkdir /usr
+hdfs dfs -mkdir /user
 hdfs dfs -mkdir /data
 
 echo LIST ROOT DIRECTORY TO LOOK FOR SUCCESSFUL CREATE OF NEW DIRECTORIES
