ee7e43fadcd2582eb496ba08214f6249b2bae98c
alkirej <jeff@alkire.net>
2020-01-30 17:31:00 -0500

    change scripts to read config files from current location instead of flume installation dirs

--- 011-FlumeExecSeq/run_seq.sh
+++ 011-FlumeExecSeq/run_seq.sh
@@ -1,5 +1,5 @@
 
-cp /home/jeff/BigData/011-FlumeExecSeq/seq.conf /opt/flume/conf
+# cp /home/jeff/BigData/011-FlumeExecSeq/seq.conf /opt/flume/conf
 
-/opt/flume/bin/flume-ng agent -c /opt/flume/ -f /opt/flume/conf/seq.conf --name SeqAgent -Dflume.root.looger=INFO,console
+/opt/flume/bin/flume-ng agent -c /opt/flume/ -f /home/jeff/BigData/011-FlumeExecSeq/seq.conf --name SeqAgent -Dflume.root.looger=INFO,console
 
