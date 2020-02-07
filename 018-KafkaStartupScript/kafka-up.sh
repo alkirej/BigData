94850f7e238daf2d4aa991b1a7aa14a05abf6d54
alkirej <jeff@alkire.net>
2020-02-07 18:19:42 -0500

    work on kafka startup script

--- 018-KafkaStartupScript/kafka-up.sh
+++ 018-KafkaStartupScript/kafka-up.sh
@@ -10,6 +10,25 @@ else
     fi   
 fi
 
+if [ -z $KAFKA_HOME ]
+
+then
+    echo
+    echo Unable to comply: KAFKA_HOME environment variable is not set.
+    echo
+    exit 2
+fi
+
+if [ -r $KAFKA_HOME/config/server.properties ]
+then
+else
+    echo 
+    echo Unable to comply: $KAFKA_HOME/config/server.properties 
+    echo does not exist or is unable to be read.
+    echo 
+    exit 3
+fi
+
 if [ -z $VALID ]
 then
     echo "kafka-up.sh:  start zookeeper and a specified number of broker daemons"
@@ -18,41 +37,25 @@ then
     echo "    where <broker-count> is a number between 4 and 7"
     echo "" 
     exit 1    
-else
-    zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties
-    for ((i=1; c<=$VALID; i++))
-    do
-      cat /opt/kafka/config/server.properties | \
-         sed -e "s/log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-$VALID	/" \
-         > server.properties.$VALID 
-      kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties.$VALID
 
-echo -n "$c"
-        sleep 1
+else
+    echo
+    echo ... Starting Zookeeper ....
+    echo
+    zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties
+
+    for ((i=1; i<=${VALID}; i++))
+    do
+        cat $KAFKA_HOME/config/server.properties | \
+            sed -e "s/log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-${i}/" | \
+            sed -e "s/broker.id=0/kafka-logs-${i}/" \
+                > $KAFKA_HOME/config/server.properties.${i}
+
+    echo ... Starting Broker ${i} ....
+    kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties.${i}
     done
 fi
 
-exit 0
-
-sleep 1
-cd /opt/kafka/config
-
-cat /opt/kafka/config/server.properties | sed -e "s/log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-1/"> server.properties.1
-echo "
-listeners=PLAINTEXT://:19091
-" >> server.properties.1
-
-cat /opt/kafka/config/server.properties | sed -e "s/log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-2/"> server.properties.2
-echo "
-listeners=PLAINTEXT://:19092
-" >> server.properties.2
-
-
-kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties.1
-sleep 1
-kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties.2
-sleep 1
-
 # create a sample topic
-kafka-topics.sh --create --topic example --zookeeper localhost:2181 --partitions 1 --replication-factor 1
+#kafka-topics.sh --create --topic example --zookeeper localhost:2181 --partitions 1 --replication-factor 1
 
