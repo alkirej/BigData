17d0f9b878d4658d85f4151d6da136d7e1106fb1
Jeff Alkire <jeff@alkire.net>
2020-02-14 13:09:29 -0500

    store results to hdfs

--- 016-KafkaProdConPython/KafkaServerInfo.py
+++ 016-KafkaProdConPython/KafkaServerInfo.py
@@ -1,4 +1,7 @@
-# DO NOT CHANGE IN CODE !!!
-KAFKA_BROKER_ADDR = "localhost:50001"
-TOPIC_NAME        = "GlobalWarming"
-CHAR_ENCODING     = "utf-8"
+
+class KafkaServerInfo:
+    # DO NOT CHANGE IN CODE !!!
+    KAFKA_BROKER_ADDR   = "localhost:50001"
+    KAFKA_BROKER_ADDR_2 = "jeff-laptop-2:50001"
+    TOPIC_NAME          = "GlobalWarming"
+    CHAR_ENCODING       = "utf-8"
--- 016-KafkaProdConPython/kafka_consumer.py
+++ 016-KafkaProdConPython/kafka_consumer.py
@@ -1,13 +1,26 @@
+import os
 from kafka import KafkaConsumer
-import KafkaServerInfo
+from KafkaServerInfo import KafkaServerInfo
+
+FILE_NAME   = "kafka_input.txt"
+HDFS_DIR    = "/data"
+HADOOP_HOME = "/opt/hadoop"
 
 consumer:KafkaConsumer = KafkaConsumer(
      KafkaServerInfo.TOPIC_NAME,
-     bootstrap_servers=[KafkaServerInfo.KAFKA_BROKER_ADDR],
+     bootstrap_servers=[KafkaServerInfo.KAFKA_BROKER_ADDR_2],
      auto_offset_reset='earliest'
 )
 
+m = ""
 for message in consumer:
-    print( message.value.decode(KafkaServerInfo.CHAR_ENCODING) )
-
+    m += message.value.decode(KafkaServerInfo.CHAR_ENCODING) + "\n"
+    print( m )
+    with open( FILE_NAME , 'wb' ) as f:
+        f.write(m.encode("utf-8"))
+    print( "hdfs dfs -put " + FILE_NAME + " " + HDFS_DIR + "/" + FILE_NAME )
+    ec:int = os.system( 'sh -c "' + HADOOP_HOME + '/bin/hdfs dfs -put -f ' + FILE_NAME + ' ' + HDFS_DIR + '/' + FILE_NAME + '"' )
+    print( "\nexit code: " + str(ec) + "\n" )
+        
 consumer.close()
+
