aedd1fac85b5c0bb53f9d79dbd166dbcf1365077
Jeff Alkire <jeff@alkire.net>
2020-02-21 23:34:16 -0500

    updates to kafka_producer to improve testing for future tasks

--- 016-KafkaProdConPython/KafkaServerInfo.py
+++ 016-KafkaProdConPython/KafkaServerInfo.py
@@ -1,7 +1,7 @@
 
 class KafkaServerInfo:
     # DO NOT CHANGE IN CODE !!!
-    KAFKA_BROKER_ADDR   = "localhost:50001"
-    KAFKA_BROKER_ADDR_2 = "jeff-laptop-2:50001"
+    KAFKA_BROKER_ADDR   = "jeff-laptop-apt:50001"
+    KAFKA_BROKER_ADDR_2 = "jeff-laptop-apt:50001"
     TOPIC_NAME          = "GlobalWarming"
     CHAR_ENCODING       = "utf-8"
--- 016-KafkaProdConPython/kafka_producer.py
+++ 016-KafkaProdConPython/kafka_producer.py
@@ -3,16 +3,18 @@ from names import get_full_name
 from kafka import KafkaProducer
 from names import get_full_name
 
-import KafkaServerInfo
+from KafkaServerInfo import KafkaServerInfo
 
 
-MESSAGE_COUNT: int  = 10
+MESSAGE_COUNT: int  = 30 
 
 producer: KafkaProducer = KafkaProducer( bootstrap_servers=[KafkaServerInfo.KAFKA_BROKER_ADDR]  )
 
 for i in range(MESSAGE_COUNT):
-    producer.send(KafkaServerInfo.TOPIC_NAME, get_full_name().encode(KafkaServerInfo.CHAR_ENCODING ))
-    sleep(2)
+    name: str = get_full_name()
+    producer.send(KafkaServerInfo.TOPIC_NAME, name.encode(KafkaServerInfo.CHAR_ENCODING ))
+    print( name )
+    sleep(0.5)
 
 producer.flush()
 producer.close()
