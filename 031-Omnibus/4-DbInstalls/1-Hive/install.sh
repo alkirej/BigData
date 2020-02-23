df9a0cde0f46f7e0d6cc08c8545ddf6787e0a20a
Jeff Alkire <jeff@alkire.net>
2020-02-22 23:10:34 -0500

    final hive/derby install script

--- 031-Omnibus/4-DbInstalls/1-Hive/install.sh
+++ 031-Omnibus/4-DbInstalls/1-Hive/install.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # BEFORE INSTALLING HIVE, JAVA AND HADOOP MUST BE INSTALLED
-
+##################################################################
+# HADOOP MUST BE RUNNING BEFORE RUNNING THIS INSTALL SCRIPT !!!! #
+##################################################################
 # DOWNLOAD INSTALL FILES
 cd /opt
 sudo wget http://apache.claz.org/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz
 sudo tar xvf apache-hive-3.1.2-bin.tar.gz
 sudo chown -R `whoami` apache-hive-3.1.2-bin
 sudo ln -s apache-hive-3.1.2-bin hive
-# rm apache-hive-3.1.2-bin.tar.gz
+sudo rm apache-hive-3.1.2-bin.tar.gz
 
 # SETUP HIVE ENVIRONMENT
 cd ~
-export HIVE_HOME=/usr/local/hive
+export HIVE_HOME=/opt/hive
 export PATH=$PATH:$HIVE_HOME/bin
-export CLASSPATH=.:/usr/local/Hadoop/lib/*:/usr/local/hive/lib/*
+export CLASSPATH=.:$HIVE_HOME/Hadoop/lib/*:$HIVE_HOME/hive/lib/*
 
 cd $HIVE_HOME/conf
 cp hive-env.sh.template hive-env.sh
@@ -28,19 +30,19 @@ sudo wget http://archive.apache.org/dist/db/derby/db-derby-10.4.2.0/db-derby-10.
 sudo tar xvf db-derby-10.4.2.0-bin.tar.gz
 sudo chown -R `whoami` db-derby-10.4.2.0-bin
 sudo ln -s db-derby-10.4.2.0-bin derby
-rm db-derby-10.4.2.0-bin.tar.gz
+sudo rm db-derby-10.4.2.0-bin.tar.gz
 
 # SETUP DERBY ENVIRONMENT
-export DERBY_HOME=/usr/local/derby
+export DERBY_HOME=/opt/derby
 export PATH=$PATH:$DERBY_HOME/bin
 export CLASSPATH=$CLASSPATH:$DERBY_HOME/lib/derby.jar:$DERBY_HOME/lib/derbytools.jar
 
 
 echo "
-export HIVE_HOME=/usr/local/hive
-export DERBY_HOME=/usr/local/derby
+export HIVE_HOME=$HIVE_HOME
+export DERBY_HOME=/opt/derby
 export PATH=$PATH:$HIVE_HOME/bin:$DERBY_HOME/bin
-export CLASSPATH=.:/usr/local/Hadoop/lib/*:/usr/local/hive/lib/*:$DERBY_HOME/lib/derby.jar:$DERBY_HOME/lib/derbytools.jar
+export CLASSPATH=.:$HADOOP_HOME/lib/*:$HIVE_HOME/lib/*:$DERBY_HOME/lib/derby.jar:$DERBY_HOME/lib/derbytools.jar
 " >> ~/.bash_profile
 
 
@@ -62,7 +64,7 @@ org.jpox.transactionIsolation = read_committed
 javax.jdo.option.DetachAllOnCommit = true
 javax.jdo.option.NontransactionalRead = true
 javax.jdo.option.ConnectionDriverName = org.apache.derby.jdbc.ClientDriver
-javax.jdo.option.ConnectionURL = jdbc:derby://localhost:1527/metastore_db;
+javax.jdo.option.ConnectionURL = jdbc:derby://localhost:1527/metastore_db;create=true;
 javax.jdo.option.ConnectionUserName = app
 javax.jdo.option.ConnectionPassword = derby
 " > jpox.properties
@@ -71,7 +73,7 @@ echo "
 <configuration>
    <property>
       <name>javax.jdo.option.ConnectionURL</name>
-      <value>jdbc:derby://localhost:1527/metastore_db</value>
+      <value>jdbc:derby://localhost:1527/metastore_db;create=true;</value>
       <description>metadata is stored in a Derby server</description>
    </property>
    <property>
@@ -93,5 +95,19 @@ echo "
 " > hive-site.xml
 
 # CREATE HIVE METASTORE
-hive --service metastore
+cd $JAVA_HOME/jre/lib/security
+sudo chmod 666 java.policy
+echo "
+grant {
+    permission java.net.SocketPermission \"localhost:1527\", \"listen,resolve\";
+};
+" >> java.policy
+sudo chmod 644 java.policy
+
+cd $DERBY_HOME/bin
+./startNetworkServer &
+sleep 5s
+
+echo "Create hive metastore"
+schematool -dbType derby -initSchema
 
