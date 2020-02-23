54abf52a1a03b7e8bd8f3048d77550c81dc20d61
Jeff Alkire <jeff@alkire.net>
2020-02-23 11:15:50 -0500

    latest draft of cassandra install

--- 031-Omnibus/4-DbInstalls/3-Cassandra/install.sh
+++ 031-Omnibus/4-DbInstalls/3-Cassandra/install.sh
@@ -4,16 +4,18 @@
 # AFTER INSTALL, execute the following command to connect to cassandra server:
 # cqlsh
 
-sudo apt install curl
-
 sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E91335D77E3E87CB
 
 echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
 sudo wget https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 
+echo "Y
+" > Yes.txt
 sudo apt-get update
 sudo apt-get install cassandra
 
+rm Yes.txt
+rm KEYS
 
 
