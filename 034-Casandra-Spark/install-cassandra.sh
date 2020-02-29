f1d21d0c1120249459d4c6036d9155b9d716f179
Jeff Alkire <jeff@alkire.net>
2020-02-29 18:38:52 -0500

    begin work on task 34. save progress for dinner.

---
+++ 034-Casandra-Spark/install-cassandra.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /opt
+# sudo wget http://mirrors.sonic.net/apache/cassandra/2.1.21/apache-cassandra-2.1.21-bin.tar.gz
+# sudo tar -xzvf apache-cassandra-2.1.21-bin.tar.gz
+sudo ln -s /opt/apache-cassandra-2.1.21-bin /opt/cassandra
+
+# rm apache-cassandra-2.1.21-bin.tar.gz
+
