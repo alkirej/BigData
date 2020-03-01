b0dabed217810c5e86024b41e3e00980cb70cb1d
Jeff Alkire <jeff@alkire.net>
2020-02-29 22:30:47 -0500

    save latest changes

---
+++ 034-Casandra-Spark/Task34Constants.py
@@ -0,0 +1,15 @@
+"""
+Author: Jeff Alkire
+Date:   02-24-20
+Notes:  Store constants for Task 32 project. Read a csv and json file from the disk and store
+        it to a hive table.
+"""
+class Task32Constants:
+    CSV_FILE_NAME  = "migrantsRefugees.csv"
+    JSON_FILE_NAME = "simpleSample.json"
+
+    # MACHINE_DEPENDANT = ""
+    MACHINE_DEPENDANT = "main/"
+
+    CSV_FILE_URL  = "file:///home/jeff/" + MACHINE_DEPENDANT + "git/BigData/034-Cassandra-Spark/" + CSV_FILE_NAME
+    JSON_FILE_URL = "file:///home/jeff/" + MACHINE_DEPENDANT + "git/BigData/034-Cassandra-Spark/" + JSON_FILE_NAME
