6e9d35fcfd739613375d88f821239198808359b7
Jeff Alkire <jeff@alkire.net>
2020-03-01 20:06:30 -0500

    add rss feeds to data pull

--- 036-Capstone-Presentation/data_pull/Constants.py
+++ 036-Capstone-Presentation/data_pull/Constants.py
@@ -7,7 +7,14 @@ class Constants:
         'x-rapidapi-key': "1f01528fb6msh561539a23d5f12ep158231jsna853de0e02d6"
         }
 
-    URL_TEAM_INFO: str   = "https://api-nba-v1.p.rapidapi.com/teams/teamId/"
-    URL_PLAYER_LIST: str  = "http://data.nba.net/10s/prod/v1/2019/players.json"
+    # URL_TEAM_INFO: str    = "https://api-nba-v1.p.rapidapi.com/teams/teamId/"
+    # URL_PLAYER_LIST: str  = "http://data.nba.net/10s/prod/v1/2019/players.json"
 
     URL_GAMES_ON_DAY: str = "https://api-nba-v1.p.rapidapi.com/games/date/"
+
+    RSS_URLS: list = [ "https://api.foxsports.com/v1/rss?tag=nba",
+                       "https://www.espn.com/espn/rss/nba/news"
+                     ]
+
+    GAME_DATA_DIR: str = "data/"
+    RSS_DATA_DIR: str  = "rss_data/"
--- 036-Capstone-Presentation/data_pull/DataPull.py
+++ 036-Capstone-Presentation/data_pull/DataPull.py
@@ -1,12 +1,25 @@
 """
 
 """
+import datetime
+
 import requests
 import json
 from requests.models import Response
 
 from Constants import Constants
 
+"""
+def build_simple_api_call( url: str, id: int ) -> str:
+    return url + str(id)
+
+def get_team_info( id: int ) -> str:
+    team_info_url: str = build_simple_api_call( Constants.URL_TEAM_INFO, id )
+    response: Response = requests.request( Constants.HTTP_GET, team_info_url, headers=Constants.HEADERS )
+    return json.dumps( response.json() )
+"""
+
+# ROUTINES TO READ GAME DATA FROM API AND SAVE IT TO DISK
 def build_date_api_call( url: str, year: int, month: int, day: int ) -> str:
     # ASSERTIONS ABOUT INPUT
     assert( year < 2100 )
@@ -37,22 +50,54 @@ def build_date_api_call( url: str, year: int, month: int, day: int ) -> str:
     date_str: str = year_str + "-" + month_str + "-" + day_str
     return url + date_str
 
-def build_simple_api_call( url: str, id: int ) -> str:
-    return url + str(id)
-
-def get_team_info( id: int ) -> str:
-    team_info_url: str = build_simple_api_call( Constants.URL_TEAM_INFO, id )
-    response: Response = requests.request( Constants.HTTP_GET, team_info_url, headers=Constants.HEADERS )
-    return json.dumps( response.json() )
-
 def get_games_for( year: int, month: int, day: int ):
     games_by_day_url: str = build_date_api_call( Constants.URL_GAMES_ON_DAY, year, month, day )
     response: Response = requests.request( Constants.HTTP_GET, games_by_day_url, headers=Constants.HEADERS)
     return json.dumps( response.json() )
 
+def get_yesterdays_games():
+    yes: datetime = datetime.datetime.today() - datetime.timedelta(days=1)
+    return get_games_for( yes.year, yes.month, yes.day )
 
-output: str = get_games_for( 2020, 2, 28 )
-file = open(r"data/2020-02-28.json","w+")
-file.write(output)
-file.close()
-print( output )
+def save_yesterdays_games_to_disk( file_contents: str ):
+    yes: datetime = datetime.datetime.today() - datetime.timedelta(days=1)
+    filename: str = build_date_api_call( Constants.GAME_DATA_DIR, yes.year, yes.month, yes.day )
+    file = open( filename, "w+" )
+    file.write(file_contents)
+    file.close()
+
+# ROUTINES TO READ AN RSS FILE OFF THE WEB AND SAVE IT TO DISK
+def read_rss_url( url: str ):
+    response: Response = requests.request( Constants.HTTP_GET, url )
+    return response.text
+
+def save_rss_to_disk( info: str, url: str, idx: int ):
+    yes: datetime = datetime.datetime.today()
+    filename: str = build_date_api_call( Constants.RSS_DATA_DIR, yes.year, yes.month, yes.day ) +\
+                                    "_" + str(idx) +\
+                                    ".rss"
+    
+    file = open( filename, "w+" )
+    file.write( info )
+    file.close()
+
+
+print( " ---- Begin  ---- " )
+print( " -- RSS Feeds  -- " )
+count: int = 0
+for url in Constants.RSS_URLS:
+    count += 1
+    text: str = read_rss_url( url )
+    save_rss_to_disk( text, url, count )
+    print( text )
+    print( "\n" )
+
+# GET DATA FOR YESTERDAY'S GAMES
+print( " -- Game Data  -- " )
+"""
+output: str = get_yesterdays_games()
+save_yesterdays_games_to_disk(output)
+"""
+
+# READ FROM RSS FEEDS
+print( " --- Complete --- " )
