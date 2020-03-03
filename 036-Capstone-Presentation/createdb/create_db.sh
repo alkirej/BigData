389f803845042d6c0104c2c4a5b40a4a1487949d
Jeff Alkire <jeff@alkire.net>
2020-03-03 15:12:00 -0500

    create directories for backup data during db creation

--- 036-Capstone-Presentation/createdb/create_db.sh
+++ 036-Capstone-Presentation/createdb/create_db.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 hive < db_setup.hql
+
+hdfs dfs -mkdir -p /data
+hdfs dfs -mkdir -p /data/game_data
+hdfs dfs -mkdir -p /data/rss_data
+
+hdfs dfs -mkdir -p /data/backup
+hdfs dfs -mkdir -p /data/backup/game_data
+hdfs dfs -mkdir -p /data/backup/rss_data
+
+
