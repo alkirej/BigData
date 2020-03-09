f6273d2925f013c41d8a78c8b21f07bf0c8cab45
Jeff Alkire <jeff@alkire.net>
2020-03-09 10:10:05 -0400

    lastest task 35 updates

--- 035-MongoDb-Spark-Kafka/src/main/scala/net/alkire/task35/Constants.scala
+++ 035-MongoDb-Spark-Kafka/src/main/scala/net/alkire/task35/Constants.scala
@@ -1,6 +1,35 @@
 package net.alkire.task35
 
-class Constants
-{
+object Constants {
+    // CREATE SPARK SESSION
+    val Master: String        = "local[*]"
+    val AppName: String       = "task33"
     
+    // CREATE KAFKA STREAM
+    val KafkaFormat: String   = "kafka"
+    
+    val ConfigServers: String   = "kafka.bootstrap.servers"
+    val ConfigSubscribe: String = "subscribe"
+    
+    val ServerList: String    = "cleanos:50001,cleanos:50002"
+    val TopicLIst: String     = "GlobalWarming,Tornado"
+    
+
+/*
+    // WRITE DF
+    val WriteOptionPath: String   = "path"
+    val WriteOptionCPLoc: String  = "checkpointLocation"
+    val WriteOptionOffset: String = "starting.offsets"
+    
+    val WriteValuePath: String    = "task33-out"
+    val WriteValueCPLoc: String   = "checkpoint"
+    val WriteValueOffset: String  = "earliest"
+    
+    val WriteFormat: String       = "csv"
+    val WriteQueryName: String    = "task-033"
+    
+    val TableName: String         = "try2"
+    val ColFamName : String       = "name"
+    val ColName : String          = "f_and_l_name"
+  */
 }
--- 035-MongoDb-Spark-Kafka/src/main/scala/net/alkire/task35/KafkaToMongoDb.scala
+++ 035-MongoDb-Spark-Kafka/src/main/scala/net/alkire/task35/KafkaToMongoDb.scala
@@ -1,6 +1,28 @@
 package net.alkire.task35
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, SparkSession, Row}
+
+
 object KafkaToMongoDb
 {
+    def main( args: Array[ String ] ): Unit =
+    {
+        Logger.getLogger("org").setLevel(Level.WARN)
     
+        // BUILD A SPARK SESSION
+        val spark: SparkSession = SparkSession.builder
+            .master(Constants.Master)
+            .appName(Constants.AppName)
+            .getOrCreate()
+    
+        // CREATING A STREAMING DATAFRAME FROM A KAFKA SOURCE
+        val df: DataFrame = spark.readStream
+            .format(Constants.KafkaFormat)
+            .option(Constants.ConfigServers, Constants.ServerList)
+            .option(Constants.ConfigSubscribe, Constants.TopicLIst)
+            .load()
+            .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+            .as("msg")
+    }
 }
