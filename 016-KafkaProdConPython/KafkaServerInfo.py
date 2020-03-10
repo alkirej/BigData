b1161dfa0f0ae9fadedeb6b410cdf24373a15d2b
Jeff Alkire <jeff@alkire.net>
2020-03-10 14:53:15 -0400

    update README of repository root

--- 016-KafkaProdConPython/KafkaServerInfo.py
+++ 016-KafkaProdConPython/KafkaServerInfo.py
@@ -1,7 +1,7 @@
 
 class KafkaServerInfo:
     # DO NOT CHANGE IN CODE !!!
-    KAFKA_BROKER_ADDR   = "jeff-laptop-apt:50001"
-    KAFKA_BROKER_ADDR_2 = "jeff-laptop-apt:50001"
+    KAFKA_BROKER_ADDR   = "cleanos:50001"
+    KAFKA_BROKER_ADDR_2 = "cleanos:50001"
     TOPIC_NAME          = "GlobalWarming"
     CHAR_ENCODING       = "utf-8"
