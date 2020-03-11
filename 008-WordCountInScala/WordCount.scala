e0edf89f2474e5438eb8e90520fa8fca1c1d88ad
Jeff Alkire <jeff@alkire.net>
2020-03-11 15:21:42 -0400

    task 8 final comments of .scala code

--- 008-WordCountInScala/WordCount.scala
+++ 008-WordCountInScala/WordCount.scala
@@ -1,8 +1,12 @@
 import java.io.File
 import java.io.PrintWriter
 
+/**
+ * Singleton executable object for task 8 - WordCount
+ */
 object WordCount {
-    val textFile = scala.io.Source.fromFile( "/home/jeff/BigData/004-WordCount/shakespeare-hamlet.txt")
+    // FILE NAME, CONTENTS, ETC.
+    val textFile = scala.io.Source.fromFile( "/home/jeff/git/BigData/004-WordCount/shakespeare-hamlet.txt")
     val contents = try
                        textFile.mkString
                    finally
@@ -10,20 +14,26 @@ object WordCount {
 
     val wordlist = contents.split( "\\W+" )
 
+    /**
+     * Main executable entry point.
+     *
+     * @param  args - command line arguments (that are ignored)
+     */
     def main( args: Array[String] ): Unit = {
         val countMap: scala.collection.mutable.Map[String,Int] = scala.collection.mutable.Map()
 
-        // count words
+        // COUNT WORDS
         for ( word <- wordlist ) {
+            // compare without regard to case
             val lcWord = word.toLowerCase
             if ( lcWord.length>0 ) {
                 val count = countMap.get( lcWord ).getOrElse(0) + 1
                 countMap += ( lcWord -> count )
-            }
+            } // if
         } // for
 
-        // output
-        val fileWriter = new PrintWriter( new File("/home/jeff/output/wordcount_scala.csv") )
+        // WRITE RESULTS TO FILE
+        val fileWriter = new PrintWriter( new File("/home/jeff/git/BigData/008-WordCountInScala/wordcount_scala.csv") )
         fileWriter.write( "word,count\n" )
 
         countMap foreach( item => fileWriter.write( item._1 + "," + item._2 + "\n" ) )
---
+++ 008-WordCountInScala/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Excuting ...
+scala /home/jeff/git/BigData/008-WordCountInScala/WordCount.scala
+echo Output written to /home/jeff/git/BigData/008-WordCountInScala/wordcount_scala.csv
