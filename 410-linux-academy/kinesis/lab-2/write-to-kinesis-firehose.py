7127d355d2b055bb81c7e9b4df52fc1da13be335
alkirej <jeff@alkire.net>
2020-04-23 16:30:28 -0700

    Naming convention

--- 410-linux-academy/kinesis/lab-2/write-to-kinesis-firehose.py
+++ 410-linux-academy/kinesis/lab-2/write-to-kinesis-firehose.py
@@ -1,41 +1,67 @@
 import boto3
-import json
-from datetime import datetime
+from faker import Faker
+import random
 import time
+import json
 
-stream_name = 'kinesis-1'
+DeliveryStreamName = 'linuxacademy-courses'
 
-kinesis_client = boto3.client('kinesis', region_name='us-east-1')
+client = boto3.client('firehose')
 
-## describe_stream to get the shard
-response = kinesis_client.describe_stream(StreamName=stream_name)
-shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+fake = Faker()
 
-shard_iterator = kinesis_client.get_shard_iterator(
-    StreamName=stream_name,
-    ShardId=shard_id,
-    ShardIteratorType='TRIM_HORIZON'
-)
+courses = [
+    "Linux+ LPIC Level 1 Exam 1",
+    "Linux+ LPIC Level 1 Exam 2",
+    "LPIC Level 2 Exam 201",
+    "Mastering The Linux Command Line",
+    "AWS Certified Developer - Associate Level ",
+    "AWS Certified SysOps Administrator - Associate Level",
+    "Introduction To Android Development",
+    "OpenStack Essentials",
+    "Learning Chef DevOps Deployment",
+    "Docker Deep Dive ",
+    "Learning Puppet DevOps Deployment (Puppet Professional Cert)",
+    "AWS Certified DevOps Engineer - Professional Level",
+    "Git and Git lab - Start to Finish",
+    "Learning Vagrant ",
+    "Linux Essentials Certification",
+    "Linux Academy Red Hat Certified Systems Administrator Prep Course",
+    "Nginx And The LEMP Stack",
+    "Linux Security Essentials",
+    "Jenkins and Build Automation",
+    "Linux Foundation Certified Systems Administrator (Legacy)",
+    "AWS Certified Solutions Architect - Associate Level",
+    "AWS Certified Solutions Architect - Professional Level",
+    "DevOps Essentials",
+    "OpenStack MCA100 - Associates Certification",
+    "Apache Tomcat 8 Application Server",
+    "Deploying MariaDB Or MySQL On VPC EC2 From Scratch With Replication",
+    "Linux Academy Red Hat Certified Engineer Prep Course",
+    "SQL Primer",
+    "Apache JBOSS Administration",
+    "Learning Python 2.7 Development",
+    "Machine Learning with Azure",
+    "Amazon Machine Learning",
+    "Azure IoT Essentials",
+    "Nagios Certified Professional"
+]
 
-my_shard_iterator = shard_iterator['ShardIterator']
-
-record_response = kinesis_client.get_records(
-    ShardIterator=my_shard_iterator,
-    Limit=1
-)
 
 
-while 'NextShardIterator' in record_response:
-    record_response = kinesis_client.get_records(
-        ShardIterator=record_response['NextShardIterator'],
-        Limit=1
+
+record = {}
+while True:
+    record['user'] = fake.name()
+    record['course'] = random.choice(courses)
+    record['video'] = random.randint(1, 10)
+    record['timestamp'] = time.time()
+    response = client.put_record(
+        DeliveryStreamName=DeliveryStreamName,
+        Record={
+            'Data': json.dumps(record)
+        }
     )
-    try:
-        print record_response['Records'][0]['Data']
-    except Exception:
-        print "No data yet."
+    print('PUTTING RECORD TO KINESIS: \n' + str(record))
+[cloud_user@ip-10-0-0-134 ~]$ 
 
-
-    time.sleep(1)
-~                                                                                           
-~                             
